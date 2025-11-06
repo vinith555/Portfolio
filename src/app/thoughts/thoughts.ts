dd82527c8054738e18ca86869edd579d99566462
unknown <vinithmuthusamy555@gmail.com>
2025-11-06 12:27:36 +0530

    modified thoughts component

--- src/app/thoughts/thoughts.ts
+++ src/app/thoughts/thoughts.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 
 @Component({
@@ -12,16 +12,32 @@ export class Thoughts {
    thoughts = [
     {
       img: 'img1.png',
-      quote: 'Nothing Is Impossible Until You Believe It Is Impossible.'
+      quote: 'Nothing Is Impossible Until You Believe It Is Impossible.',
+      clicked:false
     },
     {
       img: 'img2.png',
-      quote: 'Every Problem Has Infinite Solutions.'
+      quote: 'Every Problem Has Infinite Solutions.',
+      clicked:false
     },
     {
       img: 'img3.png',
-      quote: 'It’s Always Better To Fail Once Before You Win.'
+      quote: 'It’s Always Better To Fail Once Before You Win.',
+      clicked:false
     },
     
   ];
+  toggleThought(t: any) {
+    this.thoughts.forEach(th => {
+      if (th !== t) th.clicked = false;
+    });
+    t.clicked = !t.clicked;
+  }
+  @HostListener('document:click', ['$event'])
+  clickOutside(event: any) {
+    const clickedInside = event.target.closest('.thought-card');
+    if (!clickedInside) {
+      this.thoughts.forEach(t => (t.clicked = false));
+    }
+  }
 }
